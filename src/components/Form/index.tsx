af7ba54e3dbceebc6ac063ae4878f0e17c8f47cd
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-09-16 12:31:11 -0300

    Passando Props no form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,16 +1,30 @@
 import React from "react";
+import { ITarefa } from "../../types/tarefa";
 import Button from "../Button";
 import style from "./Form.module.scss";
 
-class Form extends React.Component {
+class Form extends React.Component <{
+  setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}>{
+  state = {
+    tarefa: "",
+    tempo: "00:00",
+  }
+
+  adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, {...this.state}])
+  }
   render() {
     return (
-      <form className={style.novaTarefa}>
+      <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">O que você quer estudar?</label>
           <input
             type="text"
             name="tarefa"
+            value={this.state.tarefa}
+            onChange={evento => this.setState({ ...this.state, tarefa: evento.target.value})}
             id="tarefa"
             placeholder="O que você quer estudar"
             required
@@ -22,13 +36,15 @@ class Form extends React.Component {
             type="time"
             step="1"
             name="tempo"
+            value={this.state.tempo}
+            onChange={evento => this.setState({ ...this.state, tempo: evento.target.value})}
             id="tempo"
             min="00:00:00"
             max="01:30:00"
             required
           />
         </div>
-        <Button >
+        <Button type='submit'>
             Adicionar
 
         </Button>
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,34 +1,24 @@
-import React from "react";
-import { isTemplateExpression } from "typescript";
+import React, {useState} from "react";
+import { ITarefa } from "../../types/tarefa";
 import Item from "./Item";
 import style from './List.module.scss'
-function List() {
-    const item = [{
-        tarefa: 'React',
-        tempo: '02:00:00',
-    }, {
-        tarefa: 'Javascript',
-        tempo: '01:00:00',
-    }, 
-    ]
+
+
+
+function List({tarefas} : { tarefas: ITarefa[]}) {
+  
   return (
     <aside className={style.listaTarefas}>
-      <h2>Estudos do dia</h2>
+      <h2 onClick={() => {
+      }}>Estudos do dia</h2>
       <ul>
-        {item.map((item, index) => (
+        {tarefas.map((item, index) => (
            <Item 
             key={index}
             {...item}
            />
         ))}
-        <li>
-          <h3> React</h3>
-          <span>02:00:00</span>
-        </li>
-        <li>
-          <h3>Javascript</h3>
-          <span>01:00:00</span>
-        </li>
+       
       </ul>
     </aside>
   );
